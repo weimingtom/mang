6e1e900265e7663bd629ce2dd6ebb79ae2c0c504
Adrian <andarms@outlook.com>
2015-04-29 20:09:00 -0500

    Add the app header

--- src/app/app.js
+++ src/app/app.js
@@ -2,25 +2,30 @@
   'use strict';
 
    angular.module( 'mang', [ 'ngMaterial' ] )
-    .config(['$mdIconProvider', function($mdIconProvider) {
-            $mdIconProvider
-                .iconSet('action', 'app/iconsets/action-icons.svg', 24)
-                .iconSet('alert', 'app/iconsets/alert-icons.svg', 24)
-                .iconSet('av', 'app/iconsets/av-icons.svg', 24)
-                .iconSet('communication', 'app/iconsets/communication-icons.svg', 24)
-                .iconSet('content', 'app/iconsets/content-icons.svg', 24)
-                .iconSet('device', 'app/iconsets/device-icons.svg', 24)
-                .iconSet('editor', 'app/iconsets/editor-icons.svg', 24)
-                .iconSet('file', 'app/iconsets/file-icons.svg', 24)
-                .iconSet('hardware', 'app/iconsets/hardware-icons.svg', 24)
-                .iconSet('icons', 'app/iconsets/icons-icons.svg', 24)
-                .iconSet('image', 'app/iconsets/image-icons.svg', 24)
-                .iconSet('maps', 'app/iconsets/maps-icons.svg', 24)
-                .iconSet('navigation', 'app/iconsets/navigation-icons.svg', 24)
-                .iconSet('notification', 'app/iconsets/notification-icons.svg', 24)
-                .iconSet('social', 'app/iconsets/social-icons.svg', 24)
-                .iconSet('toggle', 'app/iconsets/toggle-icons.svg', 24)
-        }]);
+    .config(['$mdIconProvider', '$mdThemingProvider', function($mdIconProvider, $mdThemingProvider) {
+      $mdIconProvider
+        .iconSet('action', 'app/iconsets/action-icons.svg', '20')
+        .iconSet('alert', 'app/iconsets/alert-icons.svg', '20')
+        .iconSet('av', 'app/iconsets/av-icons.svg', '20')
+        .iconSet('communication', 'app/iconsets/communication-icons.svg', '20')
+        .iconSet('content', 'app/iconsets/content-icons.svg', '20')
+        .iconSet('device', 'app/iconsets/device-icons.svg', '20')
+        .iconSet('editor', 'app/iconsets/editor-icons.svg', '20')
+        .iconSet('file', 'app/iconsets/file-icons.svg', '20')
+        .iconSet('hardware', 'app/iconsets/hardware-icons.svg', '20')
+        .iconSet('icons', 'app/iconsets/icons-icons.svg', '20')
+        .iconSet('image', 'app/iconsets/image-icons.svg', '20')
+        .iconSet('maps', 'app/iconsets/maps-icons.svg', '20')
+        .iconSet('navigation', 'app/iconsets/navigation-icons.svg', '20')
+        .iconSet('notification', 'app/iconsets/notification-icons.svg', '20')
+        .iconSet('social', 'app/iconsets/social-icons.svg', '20')
+        .iconSet('toggle', 'app/iconsets/toggle-icons.svg', '20')
+
+      $mdThemingProvider.theme('default')
+        .primaryPalette('cyan');
+
+
+    }]);
 
 
 })();
